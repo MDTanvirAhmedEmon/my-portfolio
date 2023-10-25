c867ff16b2057df50a933aa7416fc19be8e9b45c
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2023-10-26 00:30:40 +0600

    project setup successfully

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import Header from "../../shared/Header";
+
+const Home = () => {
+    return (
+        <div className="h-screen">
+            <Header></Header>
+            <div className=" bg-sky-500 h-[90%]">
+                <div className="container mx-auto">
+                    <div>
+                        <h1></h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
